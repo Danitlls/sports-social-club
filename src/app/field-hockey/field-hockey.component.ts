debcfa66ccd6007b0707fb05c2e9aaca60a92679
Daniela Trulls <danielatrulls01@gmail.com>
2017-07-14 11:01:43 -0700

    Add functionality to connect 'field-hockey' with 'player-detail'

--- src/app/field-hockey/field-hockey.component.ts
+++ src/app/field-hockey/field-hockey.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../player.model';
+import { Router } from '@angular/router';
+import { PlayerService } from '../player.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-field-hockey',
   templateUrl: './field-hockey.component.html',
-  styleUrls: ['./field-hockey.component.sass']
+  styleUrls: ['./field-hockey.component.sass'],
+  providers: [PlayerService]
 })
-export class FieldHockeyComponent implements OnInit {
 
-  constructor() { }
+export class FieldHockeyComponent implements OnInit {
+  players: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+
+  constructor(private router: Router, private playerService: PlayerService) { }
 
   ngOnInit() {
+    this.players = this.playerService.getPlayers();
+    console.log(this.router.url);
   }
+
+  goToDetailPage(clickedPlayer) {
+   this.router.navigate(['players', clickedPlayer.$key]);
+ };
 
 }
