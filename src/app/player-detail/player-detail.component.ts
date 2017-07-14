990a5a3b3c26c413162110447a80963b08b693a4
Daniela Trulls <danielatrulls01@gmail.com>
2017-07-14 16:19:08 -0700

    Add styling for player-detail.component.

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -22,7 +22,7 @@ export class PlayerDetailComponent implements OnInit {
        this.playerId = urlParameters['id'];
      });
      this.playerToDisplay = this.playerService.getPlayerById(this.playerId);
-     console.log(this.playerId)
+
     }
 
 }
